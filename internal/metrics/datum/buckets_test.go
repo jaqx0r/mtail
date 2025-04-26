8ecc4ba2149397ea2061d86a3d5713a07f072cda
Jamie Wilkinson <jaq@spacepants.org>
2025-04-26 16:39:45 +1000

    test: Improve the bucket boundary test.

--- internal/metrics/datum/buckets_test.go
+++ internal/metrics/datum/buckets_test.go
@@ -10,9 +10,10 @@ import (
 	"time"
 
 	"github.com/jaqx0r/mtail/internal/metrics/datum"
+	"github.com/jaqx0r/mtail/internal/testutil"
 )
 
-func TestBucketContains(t *testing.T) {
+func TestRangeContains(t *testing.T) {
 	if err := quick.Check(func(lo, hi, val float64) bool {
 		r := &datum.Range{Min: lo, Max: hi}
 		truth := val < hi && val >= lo
@@ -38,10 +39,11 @@ func TestMakeBucket(t *testing.T) {
 		t.Errorf("count not 1, got %v", r)
 	}
 	bs := datum.GetBucketsCumByMax(b)
-	if r := datum.GetBucketsCount(b); r != bs[math.Inf(+1)] {
-		t.Errorf("Inf bucket des not equal total observation count: %v vs %v", bs[math.Inf(+1)], r)
+	expected := map[float64]uint64{
+		1: 0,
+		2: 1,
+		4: 1,
+		math.Inf(+1): 1,
 	}
-	if len(bs) != len(r)+1 {
-		t.Errorf("missing buckets from BucketsByMax: expected %d, got %v", len(r)+1, len(bs))
-	}
+	testutil.ExpectNoDiff(t, expected, bs)
 }
