fcd72e927ae88d00e1571d7c1ca56480c961412f
Jason Herbage <jherbage@splunk.com>
2025-06-27 14:21:03 +0100

    reduce the hash to see if much quicker

--- internal/logline/logline.go
+++ internal/logline/logline.go
@@ -14,12 +14,12 @@ type LogLine struct {
 	Context context.Context
 
 	Filename     string // The log filename that this line was read from
-	Filenamehash uint32 // stored for efficiency key lookup
+	Filenamehash uint16 // stored for efficiency key lookup
 	Line         string // The text of the log line itself up to the newline.
 }
 
 // New creates a new LogLine object.
 func New(ctx context.Context, filename string, line string) *LogLine {
 	hash := sha256.Sum256([]byte(filename))
-	return &LogLine{ctx, filename, binary.BigEndian.Uint32(hash[:4]), line}
+	return &LogLine{ctx, filename, binary.BigEndian.Uint16(hash[:2]), line}
 }
